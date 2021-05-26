41226281b8fe2d3bd6a27466278c41169ece41c2
janaSunrise <warriordefenderz@gmail.com>
2021-05-26 16:34:38 +0530

    Convert `EmbedPages` into utility model.

--- bot/cogs/core/help.py
+++ bot/cogs/core/help.py
@@ -4,33 +4,15 @@ import typing as t
 from collections import namedtuple
 
 from discord import Color, Embed
-from discord.ext.commands import Cog, Command, Context, Group
+from discord.ext.commands import Cog, Command, Group
 from discord.ext.commands import HelpCommand as BaseHelpCommand
 from discord.ext.commands.errors import CheckFailure
-from discord.ext.menus import ListPageSource, Menu, MenuPages
 
 from bot import Bot
+from bot.utils.pages import EmbedPages
 
 MAX_CHARACTERS = 400
 field = namedtuple("field", ("name", "value"))
-
-
-class EmbedPages(ListPageSource):
-    def __init__(self, embeds: t.List[Embed], embeds_per_page: int = 1):
-        self.embeds = embeds
-        super().__init__(embeds, per_page=embeds_per_page)
-
-    async def format_page(self, menu: Menu, embed: Embed) -> Embed:
-        """Return the stored embed for current page."""
-        max_pages = self.get_max_pages()
-        if max_pages > 1:
-            embed.set_footer(text=f"Page {menu.current_page + 1} of {max_pages}.")
-        return embed
-
-    async def start(self, ctx: Context, **menupages_kwargs) -> None:
-        """Start the pagination."""
-        pages = MenuPages(source=self, **menupages_kwargs)
-        await pages.start(ctx)
 
 
 class HelpPages(EmbedPages):
